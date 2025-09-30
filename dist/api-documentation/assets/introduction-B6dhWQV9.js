062313f1c70f54da4f79c4c05e31add494f7eea2
agustinobillgate <110078620+agustinobillgate@users.noreply.github.com>
2025-09-30 08:55:38 +0800

    Delete dist directory

--- dist/api-documentation/assets/introduction-B6dhWQV9.js
+++
@@ -1,8 +0,0 @@
-import{u as i,j as o}from"./entry.client-BLnSq4tB.js";const u="Thanks for choosing to use Zudoku to power your API documentation!",d=[{depth:2,value:"Edit this page",id:"edit-this-page"},{depth:2,value:"Configuration",id:"configuration"}],r={title:"Welcome to Zudoku",lastModifiedTime:"2025-09-29T09:07:27.891Z"},c="pages\\introduction.mdx";function e(n){const t={a:"a",code:"code",h2:"h2",p:"p",...i(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(t.p,{children:"Thanks for choosing to use Zudoku to power your API documentation!"}),`
-`,o.jsx(t.h2,{id:"edit-this-page",children:"Edit this page"}),`
-`,o.jsxs(t.p,{children:["To edit this page, open the ",o.jsx(t.code,{inline:"true",children:"pages/introduction.mdx"})," file."]}),`
-`,o.jsx(t.h2,{id:"configuration",children:"Configuration"}),`
-`,o.jsxs(t.p,{children:["You can find the full configuration for your Zudoku site in the ",o.jsx(t.code,{inline:"true",children:"zudoku.config.jsx"})," file."]}),`
-`,o.jsxs(t.p,{children:[`For further information on how to customize your site, check out the\r
-`,o.jsx(t.a,{href:"https://zudoku.dev/docs",children:"Zudoku documentation"}),"."]})]})}function a(n={}){const{wrapper:t}={...i(),...n.components};return t?o.jsx(t,{...n,children:o.jsx(e,{...n})}):e(n)}export{c as __filepath,a as default,u as excerpt,r as frontmatter,d as tableOfContents};
-//# sourceMappingURL=introduction-B6dhWQV9.js.map
